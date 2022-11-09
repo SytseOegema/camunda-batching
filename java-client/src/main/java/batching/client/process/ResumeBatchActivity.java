5ba9265ba711e7cc4062e668587ce14571f59c79
Sytse Oegema <sy.oegema@oegematransport.nl>
2022-11-09 15:53:50 +0100

    update java-client to resume processInstance

--- java-client/src/main/java/batching/client/process/ResumeBatchActivity.java
+++ java-client/src/main/java/batching/client/process/ResumeBatchActivity.java
@@ -3,6 +3,8 @@ package batching.client;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.ZeebeClientBuilder;
 import io.camunda.zeebe.client.api.response.ResumeBatchActivityResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ResumeBatchActivity {
 
@@ -10,26 +12,41 @@ public final class ResumeBatchActivity {
     final String defaultAddress = "localhost:26500";
     final ZeebeClientBuilder clientBuilder;
 
+    if (args.length < 7) {
+      System.out.println("Not enough arguments. Request cannot be executed");
+    } else {
+      final long processInstanceKey = Long.parseLong(args[0]);
+      final String bpmnProcessId = args[1];
+      final int processVersion = Integer.parseInt(args[2]);
+      final long processDefinitionKey = Long.parseLong(args[3]);
+      final String elementId = args[4];
+      final String bpmnElementType = args[5];
+      final long flowScopeKey = Long.parseLong(args[6]);
 
-    final String bpmnProcessId = "Eten";
-    if (args.length > 0) {
-      final String resourcePath = args[0];
-    }
+      clientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(defaultAddress).usePlaintext();
+      try (final ZeebeClient client = clientBuilder.build()) {
 
-    clientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(defaultAddress).usePlaintext();
-    try (final ZeebeClient client = clientBuilder.build()) {
+        System.out.println("Creating process instance");
 
-      System.out.println("Creating process instance");
-
-      final ResumeBatchActivityResponse response =
+        final ResumeBatchActivityResponse response =
         client
-          .newResumeBatchActivityCommand()
-          .bpmnProcessId(bpmnProcessId)
-          .send()
-          .join();
+        .newResumeBatchActivityCommand()
+        .isBatchExecuted(false)
+        .addProcessInstance(
+          processInstanceKey,
+          bpmnProcessId,
+          processVersion,
+          processDefinitionKey,
+          elementId,
+          bpmnElementType,
+          flowScopeKey
+        )
+        .send()
+        .join();
 
-        System.out.println(
-          "Process instance created with key: " + response.getBpmnProcessId ());
+        System.out.println("Process response:");
+        System.out.println(response.getSuccess());
+      }
     }
   }
 }
