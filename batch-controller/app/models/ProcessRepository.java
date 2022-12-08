1384c5563f00553bc17240a8c701a52aaa3f2a12
Sytse Oegema <sy.oegema@oegematransport.nl>
2022-12-08 09:53:28 +0100

    update of process get endpoint via repo

--- batch-controller/app/models/ProcessRepository.java
+++ batch-controller/app/models/ProcessRepository.java
@@ -10,6 +10,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Optional;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -17,6 +18,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import io.camunda.batching.messaging.messages.ProcessDTO;
 import io.camunda.batching.messaging.messages.ActivityDTO;
+import io.camunda.batching.messaging.messages.ActivityType;
 
 @Singleton
 public class ProcessRepository {
@@ -29,8 +31,20 @@ public class ProcessRepository {
     this.executionContext = executionContext;
   }
 
-  public CompletionStage<Optional<List<ProcessModel>>> list() {
-    String sql = "SELECT id, name FROM process";
+  public CompletionStage<Optional<List<ProcessDTO>>> list() {
+    final String sql = "SELECT "
+      + "process.id AS process_id, "
+      + "process.name AS process_name, "
+      + "activity.id AS activity_id, "
+      + "activity.name AS activity_name, "
+      + "activity.activity_type AS activity_type "
+      + " FROM process"
+      + " LEFT JOIN process_activity"
+      + " ON process_activity.process_id = process.id"
+      + " LEFT JOIN activity"
+      + " ON activity.id = process_activity.activity_id";
+
+      // wel anders
 
     return CompletableFuture.supplyAsync(
       () -> {
@@ -38,13 +52,33 @@ public class ProcessRepository {
           Connection connection = db.getConnection();
           Statement st = connection.createStatement();
           ResultSet rs = st.executeQuery(sql);
+          HashMap<String, ProcessDTO> map = new HashMap<String, ProcessDTO>();
 
-          List<ProcessModel> processes = new ArrayList<ProcessModel>();
           while(rs.next()) {
-            processes.add(new ProcessModel(rs.getString("id"), rs.getString("name")));
+            if(map.containsKey(rs.getString("process_id"))) {
+              ProcessDTO process = map.get(rs.getString("process_id"));
+              process.activities.add(new ActivityDTO(
+                rs.getString("activity_id"),
+                rs.getString("activity_name"),
+                getType(rs.getString("activity_type"))));
+              map.replace(rs.getString("process_id"), process);
+            } else {
+              List<ActivityDTO> activities = new ArrayList<ActivityDTO>();
+              activities.add(new ActivityDTO(
+                  rs.getString("activity_id"),
+                  rs.getString("activity_name"),
+                  getType(rs.getString("activity_type"))));
+
+              ProcessDTO process = new ProcessDTO(
+                rs.getString("process_id"),
+                rs.getString("process_name"),
+                activities);
+
+              map.put(rs.getString("process_id"), process);
+            }
           }
           connection.close();
-          return Optional.of(processes);
+          return Optional.of(new ArrayList<ProcessDTO>(map.values()));
         } catch(SQLException e) {
           e.printStackTrace();
         }
@@ -115,5 +149,36 @@ public class ProcessRepository {
     } catch(SQLException e) {
       e.printStackTrace();
     }
+  }
+
+  private ActivityType getType(String type) {
+    ActivityType activityType = ActivityType.UNSPECIFIED;
+    switch(type) {
+      case "serviceTask":
+        activityType = ActivityType.SERVICE_TASK;
+        break;
+      case "receiveTask":
+        activityType = ActivityType.RECEIVE_TASK;
+        break;
+      case "userTask":
+        activityType = ActivityType.USER_TASK;
+        break;
+      case "manualTask":
+        activityType = ActivityType.MANUAL_TASK;
+        break;
+      case "businessRuleTask":
+        activityType = ActivityType.BUSINESS_RULE_TASK;
+        break;
+      case "scriptTask":
+        activityType = ActivityType.SCRIPT_TASK;
+        break;
+      case "sendTask":
+        activityType = ActivityType.SEND_TASK;
+        break;
+      default:
+        activityType = ActivityType.UNSPECIFIED;
+        break;
+    }
+    return activityType;
   }
 }
